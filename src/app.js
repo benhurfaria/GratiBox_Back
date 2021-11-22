2c66d810a399bc80a906735f37a2bb8763c78fad
Ben Hur Faria <benhurfaria3@gmail.com>
2021-11-21 22:52:01 -0300

    services

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { signUp } from './controllers/sign-up.js';
 import { signIn } from './controllers/sign-in.js';
+import { userService } from './controllers/userServices.js';
 
 const app = express();
 app.use(cors());
@@ -9,5 +10,6 @@ app.use(express.json());
 
 app.post('/sign-up', signUp);
 app.post('/sign-in', signIn);
+app.post('/userServices', userService);
 
 export default app;
